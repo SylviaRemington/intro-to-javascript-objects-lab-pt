6aea1bc6beb6929ec8b9b1e2b24176ff60de6ae9
SylviaRemington <sylviaremington@gmail.com>
2025-04-09 18:10:19 -0700

    Exercise 1 & 2 done with notes

--- app.js
+++ app.js
@@ -20,18 +20,52 @@ const game = {
     ],
   }
 
-console.dir(pokemon, { maxArrayLength: null })
-// console.log(name, [59]);
-// console.log(game);
+  console.dir(pokemon, { maxArrayLength: null })
+  /*(Above, Line 23) Using a method: console.dir().
+  We’re using it here so that you can see all of the Pokemon in the console. 
+  Normally, both console.dir() and console.log() show only the first 100 items in an array. 
+  However, by using { maxArrayLength: null } with console.dir(), we can display every item in the array. 
+  This specific option isn’t available with console.log(), making console.dir() the necessary choice 
+  for the full visibility needed in this lab... 
+  See this in console log on web browser as Array(151) with drop down menu of arrays 0-99, 100-150.*/
+
+//   For exercise 1, below are the first two trys of logging the name of the Pokemon with the number 59;
+//   but the correct answer (I think) is console.log(pokemon[59].name);
+
+//   console.log(name, [59]);
+//   or
+//   console.log(game[59]);
+//   or 
+//   console.log(game[59].name);
+console.log(pokemon[59].name);//index #59 is actually pokemon 60, which is Poliwag
+console.log(pokemon[58].name);//is actually pokemon 59 at index 58, which is Arcanine
+// QUESTION - Which one is the assignment asking me for? The index or the actual pokemon number?
+
+// ---------------------------------------------------------------------------------------------------------
+
+// Exercise 2
+// game.party = all the Pokémon on my team (ones you've caught)
+// game.gyms = all the battles faced / checkpoints (faced or will face)
+// game.items = all the stuff I'm carrying (like Poké Balls, potions, etc.)
+
+console.log(game)
+
+//after console logging console.log(game), it shows this: {party: Array(0), gyms: Array(8), items: Array(3)}
+// see an empty party array [] --It will eventually hold all the Pokemon objects I've caught, and retrieved from pokemon array from exercise1
+// see 8 gym objects in one array (an array of objects)
+// see 3 items objects in one array (an array of objects)
+
+// ---------------------------------------------------------------------------------------------------------
+
+
+//Exercise 3
   
 
 
-/*
-Exercise 3
-1. Add a new property to the `game` object. Let's call it "difficulty".
-2. Choose a value for "difficulty" that you think fits the game. 
-Ex: "Easy", "Med" or "Hard". How would you assign it?
 
-Solve Exercise 3 here:
-*/
+
+
+  
+
+
 
