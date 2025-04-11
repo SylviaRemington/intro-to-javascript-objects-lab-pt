e152f90fe97d86bd4e74220c834a855dcd8eae55
SylviaRemington <sylviaremington@gmail.com>
2025-04-11 00:44:54 -0700

    second attempt at question 6

--- app.js
+++ app.js
@@ -209,7 +209,34 @@ Solve Exercise 6 here:
 //Each object in the gyms array has an index #... Maybe loop through the index number while specifying a difficulty less than 3
 //How do I do this???? ...the struggle is real... lol
 
+// const game = {
+//   party: [],
+//   gyms: [
+//     { location: "Pewter City", completed: false, difficulty: 1 },
+//     { location: "Cerulean City", completed: false, difficulty: 2 },
+//     { location: "Vermilion City", completed: false, difficulty: 3 },
+//     { location: "Celadon City", completed: false, difficulty: 4 },
+//     { location: "Fuchsia City", completed: false, difficulty: 5 },
+//     { location: "Saffron City", completed: false, difficulty: 6 },
+//     { location: "Cinnabar Island", completed: false, difficulty: 7 },
+//     { location: "Viridian City", completed: false, difficulty: 8 },
+//   ],
+//   items: [
+//     { name: "potion", quantity: 4 },
+//     { name: "pokeball", quantity: 8 },
+//     { name: "rare candy", quantity: 99 },
+//   ],
+// }
+//Second Attempt
+// for (let gym of gyms){
+//   if (difficulty < 3){
+//     game.gyms.completed(1,1,'true',)//use slice method???? I'm so confused
+//   }
+// }
+// console.log(game.gym.difficulty);//app.js:230 Uncaught ReferenceError: gyms is not defined
+// at app.js:230:17
 
+// I'm so confused....
 
 // ---------------------------------------------------------------------------------------------------------
 
