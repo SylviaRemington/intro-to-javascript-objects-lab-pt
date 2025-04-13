2471542379531586b05a48eb3b498d5ac871d92a
SylviaRemington <sylviaremington@gmail.com>
2025-04-13 00:31:41 -0700

    Exercise 10 completed

--- app.js
+++ app.js
@@ -450,3 +450,45 @@ Solve Exercise 9 here:
 })
   // ---------------------------------------------------------------------------------------------------------
 
+/*
+Exercise 10
+Create a method called `catchPokemon` and add it to the `game` object. You should not need to edit the original game object directly. This method should:
+  - Accept an object as a parameter called `pokemonObj`
+  - Add the `pokemonObj` to the `game.party` array.
+  - not return anything
+
+After writing this method, call it and pass in a Pokemon object of your choice from the `pokemon` data to catch it.
+
+Solve Exercise 10 here:
+*/
+
+// const game = {
+//   party: [],
+//   gyms: [
+//     { location: "Pewter City", completed: false, difficulty: 1 },
+//     { location: "Cerulean City", completed: false, difficulty: 2 },
+//     { location: "Vermilion City", completed: false, difficulty: 3 },
+//     { location: "Celadon City", completed: false, difficulty: 4 },
+//     { location: "Fuchsia City", completed: false, difficulty: 5 },
+//     { location: "Saffron City", completed: false, difficulty: 6 },
+//     { location: "Cinnabar Island", completed: false, difficulty: 7 },
+//     { location: "Viridian City", completed: false, difficulty: 8 },
+//   ],
+//   items: [
+//     { name: "potion", quantity: 4 },
+//     { name: "pokeball", quantity: 8 },
+//     { name: "rare candy", quantity: 99 },
+//   ],
+// }
+
+// console.dir(pokemon, { maxArrayLength: null })
+
+// game.catchPokemon = 
+
+game.catchPokemon = function(pokemonObj) {
+  game.party.push(pokemonObj);
+};
+game.catchPokemon(pokemon[25]);
+console.log(game.party);
+
+//Write explanation of this code and why chose to do it this way... when I get back to this hw tomorrow....
